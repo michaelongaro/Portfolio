71f4bdba991ad4259d54638601400b19709a5118
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-09-22 04:12:30 -0500

    to be used in refactor for mobile projects view

---
+++ src/components/projects/ProjectTitleAndIcons.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  icons: string[];
+};
+
+function ProjectTitleAndIcons(props: Props) {
+  // object for "React" -> icons filepath
+
+  // do this later
+
+  return <div>ProjectTitleAndIcons</div>;
+}
+
+export default ProjectTitleAndIcons;
