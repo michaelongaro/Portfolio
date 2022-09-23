035e0f2a73bbbe43d6ee2d2e452bb589e0e5a276
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-09-23 03:20:33 -0500

    added footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+type Props = {};
+
+function Footer({}: Props) {
+  return (
+    <div
+      style={{
+        height: "5rem",
+        backgroundColor: "#05204a",
+      }}
+    ></div>
+  );
+}
+
+export default Footer;
