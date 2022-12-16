78c75c25ab0ff0afee061b03786343b33ead4fc1
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2022-12-16 03:29:04 -0600

    github interaction + wave svg animation

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import openInNewTab from "../../util/openInNewTab";
 
 import mediumGithubLogo from "../../assets/mediumGithubLogo.png";
@@ -6,8 +7,10 @@ import classes from "./Hero.module.css";
 import "../../index.css";
 
 function Hero(props: any) {
+  const [hoveringOnGithubLogo, setHoveringOnGithubLogo] = useState(false);
+
   return (
-    <div className={`${classes.heroBackground} baseFlex`}>
+    <div className={`${classes.heroBackground} baseVertFlex`}>
       <div style={{ gap: ".5em" }} className={`${classes.hero} baseVertFlex`}>
         <div className={classes.topHero}>
           Hi, I'm <span className={classes.nameUnderline}>Michael</span>
@@ -18,17 +21,77 @@ function Hero(props: any) {
           <div className={classes.rightBracket}>/&gt;</div>
         </div>
 
-        <img
-          src={mediumGithubLogo}
-          alt={"Github Logo"}
-          className={`${classes.bottomHero} ${classes.githubLogo}`}
+        <div
+          style={{ position: "relative" }}
+          className={classes.logoContainer}
+          onMouseEnter={() => {
+            setHoveringOnGithubLogo(true);
+          }}
+          onMouseLeave={() => {
+            setHoveringOnGithubLogo(false);
+          }}
+          onTouchStart={() => {
+            setHoveringOnGithubLogo(true);
+          }}
+          onTouchEnd={() => {
+            setHoveringOnGithubLogo(false);
+          }}
           onMouseDown={(e) => {
             if (e.button === 1) {
               openInNewTab("https://github.com/michaelongaro");
             }
           }}
           onClick={() => openInNewTab("https://github.com/michaelongaro")}
-        />
+        >
+          <img
+            src={mediumGithubLogo}
+            alt={"Github Logo"}
+            className={`${classes.githubLogo} ${classes.bottomHero}`}
+          />
+
+          <svg
+            style={{ opacity: hoveringOnGithubLogo ? 0.85 : 0 }}
+            className={classes.waves}
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28"
+            preserveAspectRatio="none"
+            shapeRendering="auto"
+          >
+            <defs>
+              <path
+                id="gentle-wave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+              />
+            </defs>
+            <g className={classes.parallax}>
+              <use
+                xlinkHref="#gentle-wave"
+                x="48"
+                y="0"
+                fill="rgba(5,32,74,0.5)"
+              />
+              <use
+                xlinkHref="#gentle-wave"
+                x="48"
+                y="3"
+                fill="rgba(5,32,74,0.5)"
+              />
+              <use
+                xlinkHref="#gentle-wave"
+                x="48"
+                y="5"
+                fill="rgba(5,32,74,0.3)"
+              />
+              <use
+                xlinkHref="#gentle-wave"
+                x="48"
+                y="7"
+                fill="rgba(5,32,74, 0.1)"
+              />
+            </g>
+          </svg>
+        </div>
       </div>
     </div>
   );
