b0bd6658146bfef6f5c5f4dd8a0716bd67c4c984
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-04-17 18:11:01 -0500

    uses css instead of react for all event handling

--- src/components/scrollToTop/ScrollToTop.tsx
+++ src/components/scrollToTop/ScrollToTop.tsx
@@ -5,17 +5,7 @@ import upArrowIcon from "../../assets/upArrow.svg";
 import classes from "./ScrollToTop.module.css";
 import "../../index.css";
 
-interface IDynamicHoverStyles {
-  backgroundColor: string;
-  filter: string;
-}
-
 function ScrollToTop() {
-  const [dynamicHoverStyles, setDynamicHoverStyles] =
-    useState<IDynamicHoverStyles>({
-      backgroundColor: "rgba(255, 255, 255, 0.7)",
-      filter: "brightness(1)",
-    });
   const [scrollThresholdReached, setScrollThresholdReached] =
     useState<boolean>(false);
 
@@ -32,38 +22,17 @@ function ScrollToTop() {
     };
   }, []);
 
-  function showHoveredStyles() {
-    setDynamicHoverStyles({
-      backgroundColor: "#ed9f31",
-      filter: "brightness(1.1)",
-    });
-  }
-
-  function hideHoveredStyles() {
-    setDynamicHoverStyles({
-      backgroundColor: "rgba(255, 255, 255, 0.7)",
-      filter: "none",
-    });
-  }
-
   return (
     <button
       tabIndex={43}
       style={{
         opacity: scrollThresholdReached ? 1 : 0,
         pointerEvents: scrollThresholdReached ? "auto" : "none",
-        ...dynamicHoverStyles,
       }}
       className={`${classes.scrollToTopContainer} baseFlex`}
-      onMouseEnter={showHoveredStyles}
-      onMouseLeave={hideHoveredStyles}
-      onTouchStart={showHoveredStyles}
-      onTouchCancel={hideHoveredStyles}
-      onTouchEnd={hideHoveredStyles}
       onClick={() => {
         history.pushState(null, "", "/");
         window.scrollTo(0, 0);
-        hideHoveredStyles();
       }}
     >
       <img src={upArrowIcon} alt={"Scroll to top"} />
