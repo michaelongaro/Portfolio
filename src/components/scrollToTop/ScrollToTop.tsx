1367e0dc8985757c077a4db6e8b28a1db83389a7
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2023-04-17 17:47:16 -0500

    handles touch events

--- src/components/scrollToTop/ScrollToTop.tsx
+++ src/components/scrollToTop/ScrollToTop.tsx
@@ -1,15 +1,23 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 import upArrowIcon from "../../assets/upArrow.svg";
 
 import classes from "./ScrollToTop.module.css";
 import "../../index.css";
 
+interface IDynamicHoverStyles {
+  backgroundColor: string;
+  filter: string;
+}
+
 function ScrollToTop() {
+  const [dynamicHoverStyles, setDynamicHoverStyles] =
+    useState<IDynamicHoverStyles>({
+      backgroundColor: "rgba(255, 255, 255, 0.7)",
+      filter: "brightness(1)",
+    });
   const [scrollThresholdReached, setScrollThresholdReached] =
     useState<boolean>(false);
-
-  const upArrowRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     function handleScroll() {
@@ -24,25 +32,38 @@ function ScrollToTop() {
     };
   }, []);
 
-  function blur() {
-    upArrowRef.current?.blur();
+  function showHoveredStyles() {
+    setDynamicHoverStyles({
+      backgroundColor: "#ed9f31",
+      filter: "brightness(1.1)",
+    });
+  }
+
+  function hideHoveredStyles() {
+    setDynamicHoverStyles({
+      backgroundColor: "rgba(255, 255, 255, 0.7)",
+      filter: "none",
+    });
   }
 
   return (
     <button
       tabIndex={43}
-      ref={upArrowRef}
       style={{
         opacity: scrollThresholdReached ? 1 : 0,
         pointerEvents: scrollThresholdReached ? "auto" : "none",
+        ...dynamicHoverStyles,
       }}
       className={`${classes.scrollToTopContainer} baseFlex`}
-      onMouseLeave={blur}
-      onTouchCancel={blur}
+      onMouseEnter={showHoveredStyles}
+      onMouseLeave={hideHoveredStyles}
+      onTouchStart={showHoveredStyles}
+      onTouchCancel={hideHoveredStyles}
+      onTouchEnd={hideHoveredStyles}
       onClick={() => {
         history.pushState(null, "", "/");
         window.scrollTo(0, 0);
-        blur();
+        hideHoveredStyles();
       }}
     >
       <img src={upArrowIcon} alt={"Scroll to top"} />
