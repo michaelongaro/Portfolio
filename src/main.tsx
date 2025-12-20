7a6a601ed30ec45a3d52ec3533c08fbb4d56c90e
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-12-20 01:04:39 -0600

    trying to add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Analytics from "@vercel/analytics/react";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
