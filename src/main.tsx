69484877a6e443ce6c7bdee51c9c55aa063f9c8c
Michael Ongaro <michael.ongaro.dev@gmail.com>
2025-12-20 01:09:04 -0600

    fixed import for vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Analytics from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./index.css";
 
